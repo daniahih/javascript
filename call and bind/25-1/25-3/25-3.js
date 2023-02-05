3509484bbdc3c41d9117538b8efff7a93aa74696
daniahih <daniahih310@gmail.com>
2023-02-05 14:41:14 +0200

    added comment on race exersice

--- call and bind/25-1/25-3/25-3.js
+++ call and bind/25-1/25-3/25-3.js
@@ -9,12 +9,12 @@ const wonderWoman = {
     function printName() {
         console.log(`my name is ${this.name}`);
         }
-
-        function printHeroes(heroes, printFunc) {
-            heroes.forEach(function(hero) {
-                printFunc.call(hero);
+ //  i want to do a function that takes the array and the function print 
+        function printHeroes(heroes, printFunc) { 
+            heroes.forEach(function(hero) { // hero refers to --> for each element in the array 
+                printFunc.call(hero); // call the function that will print the name  with the elements from the array  
               });
             }
-            printHeroes(superHeroes, printName);
+            printHeroes(superHeroes, printName); // superhere refers to the array hearos in the function , 
 
 
